42579ff42ef9e19fe89a331c28363ef519adf309
Simon Baird <simon.baird@gmail.com>
2021-01-06 17:05:24 -0500

    Add home controller and an index page
    Done like this:
      rails generate controller home index
    
    Also set the root route.

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root to: 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
