4c74aa3e0d16f04c2f67a699757d4eebf23ef544
Simon Baird <simon.baird@gmail.com>
2021-01-06 16:18:26 -0500

    Add devise and follow the setup instructions
    https://github.com/heartcombo/devise#getting-started
    https://johnofsydney.github.io/notes/rails/rails_social_login.html

---
+++ rails/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include devise modules. Others available are:
+  # :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable
+
+  validates_presence_of :name
+end
