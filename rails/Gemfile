e802d362a3a33866ce838b862a4e6a9697d12341
Simon Baird <simon.baird@gmail.com>
2021-01-06 16:18:26 -0500

    Add devise and follow the setup instructions
    https://github.com/heartcombo/devise#getting-started
    https://johnofsydney.github.io/notes/rails/rails_social_login.html

--- rails/Gemfile
+++ rails/Gemfile
@@ -21,6 +21,9 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+# Use devise for user management and authentication
+gem 'devise'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Initially for devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
