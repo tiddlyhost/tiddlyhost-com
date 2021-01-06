7f4a587fd029dd075a2e891114c3d3ca267117bd
Simon Baird <simon.baird@gmail.com>
2021-01-06 18:11:54 -0500

    Add bootstrap 4.5
    Not as easy as you'd think...
    
    5.0.0 is in beta now. I considered using that, but I guess we can
    upgrade later.
    
    Also installs jQuery.

--- rails/config/webpack/environment.js
+++ rails/config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+const webpack = require('webpack');
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+ $: 'jquery',
+ jQuery: 'jquery',
+ Popper: ['popper.js', 'default']
+}));
+
+const config = environment.toWebpackConfig();
+
+config.resolve.alias = {
+ jquery: 'jquery/src/jquery'
+};
+
+module.exports = environment;
