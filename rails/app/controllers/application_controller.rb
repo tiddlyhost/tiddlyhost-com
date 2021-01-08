67eaed1c1522648e8184e920020d93ee490b6ad9
Simon Baird <simon.baird@gmail.com>
2021-01-08 17:33:03 -0500

    Redirect www.example.com to example.com
    It could be done in routes.rb, but this way seemed nicer.
    
    I expect there's a way to derive the url from ssl?/domain/port, but I
    was feelin' lazy.

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_www_requests
+
+  def redirect_www_requests
+    redirect_to(Settings.home_url, status: 301) if request.subdomain == 'www'
+  end
 end
