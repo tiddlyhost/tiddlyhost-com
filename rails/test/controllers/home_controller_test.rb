7bb9a018efdb778055ef25663ed70af3177f793d
Simon Baird <simon.baird@gmail.com>
2021-01-09 09:51:34 -0500

    Add home page for signed in and signed out users

--- rails/test/controllers/home_controller_test.rb
+++ rails/test/controllers/home_controller_test.rb
@@ -4,7 +4,7 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get '/', headers: { host: Settings.main_hostname }
     assert_response :success
-    assert_match 'Home', @response.body
+    assert_select '.jumbotron h3', 'Get started now'
   end
 
   test "site subdomain routes correctly" do
