9ad7811bdda7d4f92b0556f8728aa56b34cb6e4f
Simon Baird <simon.baird@gmail.com>
2021-01-09 07:16:30 -0500

    Devise signup and login
    1. Add login/signup and logout links in navbar.
    
    2. rails g devise:views
    
    3. Add missing 'name' field to two views.
    
    4. Add controller before action to permit the name attribute.
    
    (Long story re the name field: I originally ran the devise
    initializer without specifying the name field, then thought I'd be
    clever and add it manually to the migration. Not sure what would
    have happened if I had included it to begin with, but perhaps it
    would have been the same either way.)

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
+
 class ApplicationController < ActionController::Base
   before_action :redirect_www_requests
+  before_action :permit_devise_params, if: :devise_controller?
+
+  protected
 
   def redirect_www_requests
     redirect_to(Settings.home_url, status: 301) if request.subdomain == 'www'
   end
+
+  def permit_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :todo]
+
   def index
   end
 
   def todo
   end
+
 end
