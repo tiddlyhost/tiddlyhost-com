928702b3f328ac97e65f8308397f5cd9f69fe65a
Simon Baird <simon.baird@gmail.com>
2021-01-09 07:16:30 -0500

    Devise signup and login
    1. Add login/signup and logout links in navbar.
    
    2. rails g devise:views
    
    3. Add missing 'name' field to two views.
    
    4. Add controller before action to permit the name attribute.
    
    (Long story re the name field: I originally ran the devise
    initializer without specifying the name field, then thought I'd be
    clever and add it manually to the migration. Not sure what would
    have happened if I had included it to begin with, but perhaps it
    would have been the same either way.)

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def nav_link_to(title, link, opts={})
+    content_tag :li, class: 'nav-item' do
+      link_to title, link, opts.merge(class: "nav-link#{' active' if current_page?(link)}")
+    end
+  end
 end
