23046dd1144d8b8217f3d99a180793aaa2e58410
Simon Baird <simon.baird@gmail.com>
2021-01-26 14:52:28 -0500

    Tidy up site name validator and add test
    Also make it so numerals are allowed in site names.

--- rails/test/models/site_test.rb
+++ rails/test/models/site_test.rb
@@ -2,9 +2,52 @@ require "test_helper"
 
 class SiteTest < ActiveSupport::TestCase
 
-  test "url" do
-    site = sites(:mysite)
+  setup do
+    @site = Site.find_by_name('mysite')
+  end
 
-    assert_equal 'http://mysite.example.com', site.url
+  test "url" do
+    assert_equal 'http://mysite.example.com', @site.url
+  end
+
+  test "name validation" do
+
+    # These names are invalid
+    [
+      "-aaa",
+      "aaa-",
+      "-aaa-",
+      "a--a",
+      "Aaa",
+      "aa",
+      "abc$",
+      "x" * 64,
+      "ftp",
+      "www",
+      "wiki",
+      "foo\nbar",
+      "foo\tbar",
+      "foo bar",
+    ].each do |invalid_name|
+      @site.update(name: invalid_name)
+      refute @site.valid?, "#{invalid_name} unexpectedly allowed!"
+    end
+
+    # These names are valid
+    [
+      "aaa",
+      "aa-aa",
+      "bbb-cc-dd",
+      "x" * 63,
+      "ab9",
+      "777",
+      "123-aaa",
+      "myftp",
+      "shit", # Hmm...
+    ].each do |valid_name|
+      @site.update(name: valid_name)
+      assert @site.valid?, "#{valid_name} unexpectedly disallowed!"
+    end
+
   end
 end
