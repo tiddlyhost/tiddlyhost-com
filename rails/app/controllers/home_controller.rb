fdf299081d54dd8505802308aa88f3209f4305cf
Simon Baird <simon.baird@gmail.com>
2021-01-26 12:48:32 -0500

    404 handling improvements for sites
    - Create a slim layout for the 404 page to make it look nice
    - Related refactor and cleanup in the home controller
    
    Todo: This should have some test coverage.

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -12,23 +12,25 @@ class HomeController < ApplicationController
   end
 
   def serve_tiddlywiki
-    if @site.is_public? || @site.user == current_user
+    if site_visible?
       render html: @site.tiddlywiki_file.download.html_safe, layout: nil
     else
-      render :not_found, status: 404
+      render :not_found, status: 404, layout: 'simple'
     end
   end
 
   def save_tiddlywiki
     begin
-      if user_signed_in? && @site.user == current_user
+      if site_saveable?
         @site.tiddlywiki_file.attach(params[:userfile])
         render plain: "0 - OK\n", layout: false
       else
+        # Give a 200 status no matter what so the user sees the message in a browser alert
         render plain: "If this is your site please log in at #{main_site_url} and try again.\n", layout: false
       end
     rescue => e
-      render plain: "#{e.class.name} - #{e.message}"
+      # Todo: Should probably give a generic "Save failed!" message, and log the real problem
+      render plain: "#{e.class.name} - #{e.message}", layout: false
     end
   end
 
@@ -37,6 +39,18 @@ class HomeController < ApplicationController
   end
 
   private
+
+  def site_visible?
+    @site.present? && (@site.is_public? || user_owns_site?)
+  end
+
+  def site_saveable?
+    @site.present? && user_owns_site?
+  end
+
+  def user_owns_site?
+    user_signed_in? && current_user == @site.user
+  end
 
   def find_site
     site_name = request.subdomain
