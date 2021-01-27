c81aea9344154236948a85a64f8e68532e2840ed
Simon Baird <simon.baird@gmail.com>
2021-01-27 18:00:28 -0500

    Add some test coverage for serving sites
    Test covers serving a public and private site after creating it.
    
    I don't particularly like this, (see commentary), but some testing
    is always better than no testing, right?

--- rails/test/controllers/home_controller_test.rb
+++ rails/test/controllers/home_controller_test.rb
@@ -7,15 +7,10 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select '.jumbotron h3', 'Get started now'
   end
 
-  # Fixme
-  #test "site subdomain routes correctly" do
-  #  get '/', headers: { host: "foo.#{Settings.main_hostname}" }
-  #  assert_response :success
-  #  assert_match 'todo', @response.body
-  #end
-
   test "www subdomain should redirect" do
     get home_index_url, headers: { host: "www.#{Settings.url_defaults[:host]}" }
     assert_redirected_to(Settings.url_defaults)
   end
+
+  # See also test/integration/sites_test.rb
 end
--- rails/test/integration/sites_test.rb
+++ rails/test/integration/sites_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class SitesTest < CapybaraIntegrationTest
   setup do
-    sign_in users(:bobby)
+    @user = users(:bobby)
+    sign_in @user
   end
 
   test "visiting the index" do
@@ -10,15 +11,46 @@ class SitesTest < CapybaraIntegrationTest
     assert_selector "h1", text: "Your sites"
   end
 
-  test "creating a Site" do
+  test "creating and viewing a site" do
+    # Create a site
     visit sites_url
     click_on "Create site"
-
     fill_in "site_name", with: "bar"
     click_on "Create"
 
-    # Back to the sites index after create
-    assert page.has_css? "a", text: "bar.example.com"
+    # Confirm we are sent back to sites index
+    assert_current_path '/sites'
+
+    # The index now includes a link to the new site
+    expected_url = "http://bar.example.com"
+    assert_selector %{td a[href="#{expected_url}"]}
+
+    # Visit the site and confirm it looks like a TiddlyWiki
+    click_on "bar.example.com"
+
+    # Sign out and confirm the site is still available
+    sign_out @user
+    visit expected_url
+    assert_is_tiddlywiki
+
+    # Make it a private site
+    @user.reload.sites.last.update(is_private: true)
+
+    # Confirm the private site is not available
+    visit expected_url
+    assert_is_404
+
+    # ..unless we sign in again
+    sign_in @user
+    visit expected_url
+    assert_is_tiddlywiki
+
+    # Todo:
+    # * The visit site testing should be split up and put elsewhere,
+    #   probably home_controller_test.
+    # * ..which would be easier if sites(:mysite).tiddlywiki_file.download
+    #   returned something, stubbed or otherwise.
+    # * Test coverage for saving a site.
   end
 
   test "updating a Site" do
@@ -29,7 +61,19 @@ class SitesTest < CapybaraIntegrationTest
     click_on "Update"
 
     # Back to the sites index after update
-    assert page.has_css? "a", text: "foo.example.com"
+    assert_current_path '/sites'
+    assert_selector "a", text: "foo.example.com"
+  end
+
+  def assert_is_tiddlywiki
+    assert_equal 200, page.status_code
+    assert_selector "meta[name=application-name][content=TiddlyWiki]", visible: false
+  end
+
+  def assert_is_404
+    assert_equal 404, page.status_code
+    assert_selector "title", text: "404 Not Found", visible: false
+    assert_selector "h1", text: "Not found"
   end
 
 end
