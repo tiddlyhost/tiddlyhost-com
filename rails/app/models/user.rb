6b3e1383b16e84c508cea6c2c37f95db784f615c
Simon Baird <simon.baird@gmail.com>
2021-02-12 19:20:21 -0500

    Introduce 'plans' and create two of them

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable, :trackable
 
   has_many :sites, dependent: :destroy
+  belongs_to :plan
   validates_presence_of :name
 end
