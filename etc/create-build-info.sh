3cda85f586e54544c01a48226043c57f64b42d48
Simon Baird <simon.baird@gmail.com>
2021-02-14 00:23:00 -0500

    Tweaks to build-info.txt
    Include the version and closest tag.
    
    They're a bit misleading now since I'm usually deploying builds from
    whatever is the head of origin/main but never mind that for now.

--- etc/create-build-info.sh
+++ etc/create-build-info.sh
@@ -5,7 +5,7 @@
   echo sha: `git log -n1 --format=%H`
   echo commit: `git log -n1 --format=%s`
   echo branch: `git rev-parse --abbrev-ref HEAD`
-  # Might be useful later
-  #echo tag: `git describe --tags --abbrev=0`
+  echo version: `cat rails/config/initializers/app_version.rb | cut -d"'" -f2`
+  echo tag: `git describe --tags --abbrev=0`
   echo empty: `make -s empty-version`
 ) > etc/build-info.txt
