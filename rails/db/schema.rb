aea662b033896098217fed5b0af1042007d74ed0
Simon Baird <simon.baird@gmail.com>
2021-02-18 09:02:31 -0500

    Site view counters
    ..like it's 1995.
    
    I'm not entirely sure why there's a separate HEAD request, but it
    was causing double counts.

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_004646) do
+ActiveRecord::Schema.define(version: 2021_02_18_130754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_004646) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
     t.boolean "is_searchable", default: false
+    t.integer "view_count", default: 0
     t.index ["name"], name: "index_sites_on_name", unique: true
     t.index ["user_id"], name: "index_sites_on_user_id"
   end
