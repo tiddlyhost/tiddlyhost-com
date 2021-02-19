42db3896b99bf2ba7433eb388c1332077a0d295e
Simon Baird <simon.baird@gmail.com>
2021-02-19 01:06:58 -0500

    Admin page improvements including sorting
    Default to sorting by recent activity.

--- rails/app/controllers/admin_controller.rb
+++ rails/app/controllers/admin_controller.rb
@@ -4,14 +4,20 @@ class AdminController < ApplicationController
 
   def users
     @users = User.all
+    @title = "Users"
+    @sort_col = 6
   end
 
   def sites
     if params[:user_id]
       @user = User.find(params[:user_id])
       @sites = Site.where(user: @user)
+      @title = "#{@user.name} <#{@user.email}> sites"
+      @sort_col = 7
     else
       @sites = Site.all
+      @title = "All sites"
+      @sort_col = 9
     end
   end
 
