8aedbe597f1e501b2c99dcd6ee3ff72ea2b0f522
Simon Baird <simon.baird@gmail.com>
2021-02-23 13:00:09 -0500

    Include username & use_gravatar in admin user list

--- rails/app/controllers/admin_controller.rb
+++ rails/app/controllers/admin_controller.rb
@@ -11,7 +11,7 @@ class AdminController < ApplicationController
     if params[:user_id]
       @user = User.find(params[:user_id])
       @sites = Site.where(user: @user)
-      @title = "#{@user.name} <#{@user.email}> sites"
+      @title = "#{@user.name} #{"'#{@user.username}'" if @user.has_username?} <#{@user.email}> sites"
     else
       @sites = Site.all
       @title = "All sites"
