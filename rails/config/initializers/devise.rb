e4b36e6db2d92fa6c5aaf2b41b80e6fccf14c10e
Simon Baird <simon.baird@gmail.com>
2021-02-24 10:16:20 -0500

    Increase session timeout
    30 minutes is a little too short.
    
    Also make it longer for admin user.

--- rails/config/initializers/devise.rb
+++ rails/config/initializers/devise.rb
@@ -194,6 +194,7 @@ Devise.setup do |config|
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
   # config.timeout_in = 30.minutes
+  config.timeout_in = 5.hours
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
