tiddlyhost/tiddlyhost-com

commit c079e0f0466865b1bd7eedc96f66844f019a9a5c

Don't serve a site if it doesn't look legit
It's not too hard to upload arbitrary content to Tiddlyhost. Let's
anticipate someone will try that. In this patch we add some checking
before serving a site to make sure it seems valid.

(We could also consider doing similar validations when uploading,
but it's not done here.)

It's not bullet-proof, since it wouldn't be hard to fool the
validator, but this might act as a deterrent for casual attempts.

Note the Nokogiri parser round tripping would probably make it hard
to serve non-html content, but I haven't tested that yet.

Also introduce an email support link and a new link for filing an
issue directly.

Re status code 418:
https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/418

rails/app/controllers/tiddlywiki_controller.rb
@@ -8,6 +8,9 @@ class TiddlywikiController < ApplicationController
   def serve
     return site_not_available unless site_visible?
 
+    # Don't serve a file if it doesn't resemble a valid TiddlyWiki
+    return site_not_valid unless site_valid?
+
     # Don't waste time on head requests
     return render html: '' if request.head?
 
@@ -69,6 +72,11 @@ class TiddlywikiController < ApplicationController
     @site.touch_accessed_at
   end
 
+  def site_not_valid
+    @status_code, @status_message = [418, 'Invalid TiddlyWiki']
+    render :site_not_available, status: @status_code, layout: 'simple'
+  end
+
   def site_not_available
     @status_code, @status_message = site_not_available_status
 
@@ -112,6 +120,11 @@ class TiddlywikiController < ApplicationController
 
   def site_public?
     @site.is_public?
+  end
+
+  def site_valid?
+    # Beware this requires downloading the site's content
+    @site.looks_valid?
   end
 
   def find_site

rails/app/helpers/application_helper.rb
@@ -96,4 +96,10 @@ module ApplicationHelper
      number_to_percentage(100 * number / total, opts.reverse_merge(precision: 1))
   end
 
+  def support_mail_to(opts={})
+    mail_to(Settings.support_email,
+      opts.delete(:link_title) || Settings.support_email_name,
+      opts.reverse_merge(target: '_blank'))
+  end
+
 end

rails/app/models/site.rb
@@ -82,11 +82,16 @@ class Site < ApplicationRecord
     update_column(:accessed_at, Time.now)
   end
 
+  def th_file
+    @_th_file ||= ThFile.new(tiddlywiki_file.download)
+  end
+
+  def looks_valid?
+    th_file.looks_valid?
+  end
+
   def html_content
-    @_html_content ||= begin
-      raw_html = tiddlywiki_file.download
-      ThFile.new(raw_html).apply_tiddlyhost_mods(name).to_html
-    end
+    th_file.apply_tiddlyhost_mods(name).to_html
   end
 
   def url

rails/app/views/tiddlywiki/site_not_available.html.haml
@@ -14,6 +14,15 @@
   %p
     This private site can be viewed only by its owner.
 
+- when 418
+  %p.big
+    This site does not appear to be a valid TiddlyWiki.
+  %p
+    Something has gone wrong. Please
+    = link_to('report an issue', Settings.create_issue_url, target: '_blank')
+    or send an email to
+    = support_mail_to(subject: "Site '#{@site.name}' is being classified as invalid", body: "Please take a look!") + '.'
+
 - else # 404
   %p.big
     This site doesn't exist!

rails/config/settings.yml
@@ -3,6 +3,9 @@
 
 defaults:
   issues_url: https://github.com/simonbaird/tiddlyhost/issues
+  create_issue_url: https://github.com/simonbaird/tiddlyhost/issues/new
+  support_email: tiddlyhost@gmail.com
+  support_email_name: Tiddlyhost Support
   default_plan_name: basic
   minimum_password_length: 8
 

rails/test/integration/sites_test.rb
@@ -26,6 +26,13 @@ class SitesTest < CapybaraIntegrationTest
     expected_url = "http://bar.example.com"
     assert_selector %{td a[href="#{expected_url}"]}
 
+    # Sanity check the new site
+    site = Site.last
+    assert_equal 'bar', site.name
+    assert site.is_public?
+    refute site.is_searchable?
+    assert site.looks_valid?
+
     # Visit the site and confirm it looks like a TiddlyWiki
     click_on "bar.example.com"
     assert_is_tiddlywiki
@@ -62,6 +69,7 @@ class SitesTest < CapybaraIntegrationTest
     # * ..which would be easier if sites(:mysite).tiddlywiki_file.download
     #   returned something, stubbed or otherwise.
     # * Test coverage for saving a site.
+    # * Test coverage for saving a site with invalid content.
   end
 
   test "non-existent sites" do

commit 3beca32088b50505064cbecd032e9bda80277233

Avoid additional s3 download on head request
Fix kind of a bug/regression introduced recently in c079e0f04.

Checking site validity requires fetching the site, so let's skip
that on the extra head requests performed by TiddlyWiki itself.

Also, use `head 200` which I think is the a suggested way to send an
empty body.

rails/app/controllers/tiddlywiki_controller.rb
@@ -8,11 +8,11 @@ class TiddlywikiController < ApplicationController
   def serve
     return site_not_available unless site_visible?
 
+    # Don't waste time on head requests
+    return head 200 if request.head?
+
     # Don't serve a file if it doesn't resemble a valid TiddlyWiki
     return site_not_valid unless site_valid?
-
-    # Don't waste time on head requests
-    return render html: '' if request.head?
 
     update_view_count_and_access_timestamp
 