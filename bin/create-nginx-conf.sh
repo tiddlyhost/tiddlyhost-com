483a1bb3c697bc517949fe9b4a1527c21cce5fdd
Simon Baird <simon.baird@gmail.com>
2021-03-16 00:37:40 -0400

    Add nginx container web front end
    - Update docker-compose files to add the new container.
    
    - Use envsubst to create prod and local nginx conf from
      the same template.
      The plan is to use the nginx image without a Dockerfile
      and apply all config through a mounted volume.
      (In earlier versions of this patch I was adding the config in a
      Dockerfile, but still envsubst was needed, since you can easily
      use vars in nginx config.)
    
    - No more certs in the rails container
      Nginx will deal with certs now rather than Puma. There
      are some configuration options for the proxying so Rails
      handles domains and SSL correctly. Not totally sure
      what I'm doing but there are enough blog posts and guides
      about rails and nginx out there that I think it's going
      to be okay. (Later we'll configure nginx to serve static
      rails assets directly, but it's not in this patch.)
    
    - Related ansible changes
      Need to deploy the nginx config files.
    
    - Tidy and reorder docker-compose files
      Remove some blank lines so it's easier to see it all
      in one screen.

---
+++ bin/create-nginx-conf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TEMPLATE=$1
+OUTPUT=$2
+
+if [[ "$OUTPUT" == *"prod"* ]]; then
+  TH_HOST=tiddlyhost.com
+  TS_HOST=tiddlyspot.com
+  APP_HOST=prod
+
+else
+  TH_HOST=tiddlyhost.local
+  TS_HOST=tiddlyspot.local
+  APP_HOST=base
+
+fi
+
+export TH_HOST TS_HOST APP_HOST
+envsubst '${TH_HOST} ${TS_HOST} ${APP_HOST}' < $TEMPLATE > $OUTPUT
--- docker/start-rails.sh
+++ docker/start-rails.sh
@@ -4,8 +4,4 @@
 rm -f tmp/pids/server.pid
 
 # Start rails
-if [[ "$RAILS_ENV" != "production" && "$START_WITHOUT_SSL" == "yes" ]]; then
-  exec bin/rails s -p 3333 -b '0.0.0.0'
-else
-  exec bin/rails s -p 3333 -b 'ssl://0.0.0.0?key=/opt/certs/ssl.key&cert=/opt/certs/ssl.cert'
-fi
+exec bin/rails s -p 3333 -b '0.0.0.0'
