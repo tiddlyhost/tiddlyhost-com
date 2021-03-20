0b9a696c0a5400e21949a2c7c226dc3e60610994
Simon Baird <simon.baird@gmail.com>
2021-03-20 14:21:29 -0400

    Add tiddlyspot site list to admin pages
    Also remove the extra "Owner" name field in existing sites list.
    
    Could maybe refactor to reduce duplication between the two different
    site lists, but let's worry about it later.

--- rails/app/helpers/admin_helper.rb
+++ rails/app/helpers/admin_helper.rb
@@ -2,7 +2,7 @@
 module AdminHelper
 
   def link_to_user_sites(text, user, opts={})
-    link_to(text, {controller: :admin, action: :sites, user_id: user.id}, opts)
+    link_to(text, {controller: :admin, action: params[:action], user_id: user.id}, opts)
   end
 
 end
