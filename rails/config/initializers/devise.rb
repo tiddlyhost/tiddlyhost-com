5d2c29ee8360edad2a2a340598286c9600a53b20
Simon Baird <simon.baird@gmail.com>
2021-12-30 22:11:31 -0500

    Change support email
    Also use a friendly name for the devise account confirmation from
    address.
    
    Related to #195.

--- rails/config/initializers/devise.rb
+++ rails/config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = Settings.support_email
+  config.mailer_sender = Settings.devise_mailer_sender
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
