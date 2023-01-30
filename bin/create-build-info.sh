3e4098b6169f77fde7bd7f38b1e53573d02fd04a
Simon Baird <simon.baird@gmail.com>
2023-01-30 00:34:16 -0500

    Avoid yaml error related to chore commit subject
    Hoo boy, this took way too long to debug. It's because
    build-info.txt gets parsed as yaml in the lib/settings, and having
    "chore: " in a yaml value makes the parser think it's a new key.
    
    Also replace backticks for readability.

--- bin/create-build-info.sh
+++ bin/create-build-info.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 (
-  echo date: `date`
-  echo sha: `git log -n1 --format=%H`
-  echo commit: `git log -n1 --format=%s`
-  echo branch: `git rev-parse --abbrev-ref HEAD`
+  echo date: $( date )
+  echo sha: $( git log -n1 --format=%H )
+  echo commit: "\"$( git log -n1 --format=%s )\""
+  echo branch: $( git rev-parse --abbrev-ref HEAD )
 )
