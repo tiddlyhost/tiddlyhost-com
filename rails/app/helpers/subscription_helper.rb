efb9db19885b76b6f559372f317e22501f249113
Simon Baird <simon.baird@gmail.com>
2023-02-26 10:17:29 -0500

    Add stripe customer link to user admin page
    Include some info about the subscription.
    
    Plus some related tweaks:
    - Show admin tabs above the user info instead of below
    - Move user info to a separate partial
    - Don't show filter options when showing a single user

--- rails/app/helpers/subscription_helper.rb
+++ rails/app/helpers/subscription_helper.rb
@@ -14,4 +14,26 @@ module SubscriptionHelper
     feature_list.map{ |p| Settings.plan_descriptions.dig(p, :features) || [] }.flatten
   end
 
+  def stripe_customer_url(user)
+    stripe_dashboard_url("customers", user.pay_customer_stripe.processor_id)
+  end
+
+  def stripe_dashboard_url(*path)
+    [
+      "https://dashboard.stripe.com",
+      ("test" unless Rails.env.production?),
+      *path
+    ].compact.join("/")
+  end
+
+  def subscription_renews_info(subscription)
+    if !subscription&.status == "active"
+      "inactive"
+    elsif subscription.ends_at
+      "ends in #{distance_of_time_in_words(Date.today, subscription.ends_at.to_date)}"
+    else
+      "renews in #{distance_of_time_in_words(Date.today, subscription.current_period_end.to_date)}"
+    end
+  end
+
 end
