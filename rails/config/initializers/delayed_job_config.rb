d95966566b8d30761fe2244c81837f94d36b9cc6
Simon Baird <simon.baird@gmail.com>
2023-04-17 14:38:46 -0400

    Exception notifications for delayed job errors
    Make background job failures more visible, which is more important
    now there are now background jobs triggered by stripe webhooks and I
    need to know when/if they're failing.
    
    Technique borrowed from:
    https://stackoverflow.com/questions/5972903/how-to-make-exceptionnotifier-work-with-delayed-job-in-rails-3

--- rails/config/initializers/delayed_job_config.rb
+++ rails/config/initializers/delayed_job_config.rb
@@ -1,3 +1,14 @@
 
 Delayed::Worker.logger = Logger.new(STDOUT)
 Delayed::Worker.max_run_time = 2.minutes
+
+
+# To see exception notification emails for delayed job errors
+module CustomFailedJob
+  def handle_failed_job(job, error)
+    super
+    ExceptionNotifier.notify_exception(error, data: {job: job})
+  end
+end
+
+Delayed::Worker.prepend CustomFailedJob
