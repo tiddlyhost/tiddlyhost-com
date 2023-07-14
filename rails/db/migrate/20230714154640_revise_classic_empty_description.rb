ababf98cbdb64e054d736b699098eb46b8b6b2a2
Simon Baird <sbaird@redhat.com>
2023-07-14 13:32:58 -0400

    Tweak description & tooltip for TiddlyWiki Classic
    Based on @YakvoL's changes in #307 but with some minor tweaks.

---
+++ rails/db/migrate/20230714154640_revise_classic_empty_description.rb
@@ -0,0 +1,16 @@
+class ReviseClassicEmptyDescription < ActiveRecord::Migration[7.0]
+
+  def up
+    # Should match what's in db/seeds/empties.yml
+    EmptyMigrationHelper.apply_empty_changes(%{
+      classic:
+        description: The original version of TiddlyWiki from before the 2012 "TiddlyWiki5" rewrite.
+        tooltip: Still used and developed by a small community. Fully supported on Tiddlyhost.
+    })
+  end
+
+  def down
+    # Don't try to put it back how it was
+  end
+
+end
