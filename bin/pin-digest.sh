c8a08f18dee61eebb809bb6a5be5cd7abc8c3e9d
Simon Baird <simon.baird@gmail.com>
2024-01-15 11:45:49 -0500

    Combine sequential chores into one commit
    Rather than pull-ruby, build-base, deps-update producing three commits,
    let's squash it into one.

--- bin/pin-digest.sh
+++ bin/pin-digest.sh
@@ -5,6 +5,8 @@ IMAGE_REF=$1
 DOCKER_FILE=$2
 MESSAGE=${3:-""}
 
+NO_COMMIT=${NO_COMMIT:-""}
+
 IMAGE_DIGEST="$( docker image inspect ${IMAGE_REF} --format '{{index .RepoDigests 0}}' | cut -d'@' -f2 )"
 
 REQUIRED="FROM ${IMAGE_REF}@${IMAGE_DIGEST}"
@@ -15,5 +17,8 @@ if [[ "$REQUIRED" == "$CURRENT" ]]; then
 else
   echo "Updating ${CURRENT} to ${REQUIRED}"
   sed -i "s|${CURRENT}|${REQUIRED}|" "${DOCKER_FILE}"
-  git commit "${DOCKER_FILE}" -m "chore: Bump ${IMAGE_REF/:latest/} digest" -m "Commit created with bin/pin-digest.sh" -m "$MESSAGE"
+  git add "${DOCKER_FILE}"
+  if [[ -z "$NO_COMMIT" ]]; then
+    git commit "${DOCKER_FILE}" -m "chore: Bump ${IMAGE_REF/:latest/} digest" -m "Commit created with bin/pin-digest.sh" -m "$MESSAGE"
+  fi
 fi
