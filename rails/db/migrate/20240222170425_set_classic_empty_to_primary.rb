5ea426ae7961d03231f9b433e7973efe1ef4aedc
Simon Baird <simon.baird@gmail.com>
2024-02-22 12:06:29 -0500

    Promote Classic to initially visible empty choice
    Let's bring TiddlyWiki Classic out of the shadows a little based on
    the fact that YakovL has been giving it some care and attention
    recently.
    
    (The "primary" field determines whether a particular empty should be
    shown it initially or hidden it until "Show more types" is clicked.
    Look in app/views/sites/_type_chooser.html.haml and the
    `.longer-link` handler in app/javascript/packs/application.js
    to see how that works.)

---
+++ rails/db/migrate/20240222170425_set_classic_empty_to_primary.rb
@@ -0,0 +1,18 @@
+class SetClassicEmptyToPrimary < ActiveRecord::Migration[7.1]
+
+  def up
+    # Should match what's in db/seeds/empties.yml
+    EmptyMigrationHelper.apply_empty_changes(%{
+      classic:
+        primary: true
+    })
+  end
+
+  def down
+    EmptyMigrationHelper.apply_empty_changes(%{
+      classic:
+        primary: false
+    })
+  end
+
+end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_143347) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_170425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
