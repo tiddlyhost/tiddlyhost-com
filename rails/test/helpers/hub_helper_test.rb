bf26c0142a85bbcbd95dc2870e7c0dfb4a34702c
Simon Baird <simon.baird@gmail.com>
2024-07-06 14:49:05 -0400

    rubocop: Style/NumericLiterals
    Rubocop autocorrect for the following:
    
    - Style/NumericLiterals

--- rails/test/helpers/hub_helper_test.rb
+++ rails/test/helpers/hub_helper_test.rb
@@ -6,8 +6,8 @@ class HubHelperTest < ActionView::TestCase
       123 => '123',
       1234 => '1.2K',
       1254 => '1.3K',
-      12345 => '12K',
-      123456 => '123K',
+      12_345 => '12K',
+      123_456 => '123K',
 
     }.each do |view_count, expected|
       assert_equal expected, nice_view_count(view_count)
