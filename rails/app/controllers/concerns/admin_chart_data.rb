4c3a7cabde2cd25ab4b5ca60be683c7ae96eca26
Simon Baird <simon.baird@gmail.com>
2024-07-07 12:51:32 -0400

    Add signups per-week chart for admin user
    - A new chart to show signup count per week, with the added
      benefit of doing 7 times less db queries to fetch the data
    - Some minor tweaks to the way periodic queries are prepared
    - A switch to 'week' rendering for the X axis tick marks, for
      all charts, not just the new one
    
    (I started out wanting to create a good "MAU" or "WAU" chart, but I
    don't think it's possible currently since we aren't keeping a record
    of all logins or all saves.)

--- rails/app/controllers/concerns/admin_chart_data.rb
+++ rails/app/controllers/concerns/admin_chart_data.rb
@@ -4,7 +4,7 @@ module AdminChartData
   private
 
   def chart_data_subscribers
-    day_range('2023-02-25') do |d|
+    day_range(from: SUBSCRIBERS_EPOCH) do |d|
       Pay::Subscription.where.not(status: 'canceled').
         where('created_at <= ? AND current_period_end >= ?', d, d)
     end
@@ -26,13 +26,26 @@ module AdminChartData
     end
   end
 
+  def chart_data_signups_per_week
+    # (Doesn't count deleted accounts)
+    day_range(from: USERS_EPOCH, interval: 7) do |d|
+      User.signed_in_more_than(0).
+        where('created_at >= ? AND created_at <=?', d.prev_week, d)
+    end
+  end
+
   KNOWN_CHARTS = %w[
     subscribers
     signups_per_day
+    signups_per_week
     total_users
   ].freeze
 
   DEFAULT_CHART = KNOWN_CHARTS.first
+
+  # Make these both on a Sunday so the X-axis rendering matches the data better
+  USERS_EPOCH = '2021-02-07'
+  SUBSCRIBERS_EPOCH = '2023-02-19'
 
   #---------------------------------------------------------------------------
 
@@ -45,11 +58,22 @@ module AdminChartData
     }
   end
 
-  # Fixme: This is slow and inefficient
-  def day_range(from = '2021-02-12')
-    (Date.parse(from)..Date.today).map do |d|
-      # First column is X axis, second column is Y axis
-      [d.to_s, yield(d.to_time).count]
+  # FIXME: This could probably be implemented using a single grouped
+  # db query rather than doing a separate query for each interval
+  def day_range(from: USERS_EPOCH, interval: 1)
+    start_date = Date.parse(from)
+    end_date = Date.today
+
+    x_axis = []
+    x = start_date
+    while x < end_date
+      x_axis << x
+      x += interval
+    end
+
+    x_axis.map do |x_val|
+      y_val = yield(x_val.to_time).count
+      [x_val.to_s, y_val]
     end
   end
 end
