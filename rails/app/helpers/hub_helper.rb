06113d127eb43f15f5a0e40b813ebc2f06be0941
Simon Baird <simon.baird@gmail.com>
2024-08-26 11:59:51 -0400

    Add download button and rework clone button in hub
    The UX is a little experimental. Here's what we have:
    
    - Remove right-floated clone button for cloneable sites in the hub
    - Add it back alongside a new download button with a new look and
      feel
    - In the normal hub have the buttons appear on hover, but pre-hover there
      are smaller versions so you can tell the site is cloneable. The
      idea is so the line up neatly next to the non-cloneable sites.
    - If the "show templates" hub view, all the sites have the clone and
      download buttons, so have it always visible and below the image.
    - Introduce some onhover highlighting for each site.
    - Show clone count instead of view count when looking at templates.
    
    This should resolve #323.

--- rails/app/helpers/hub_helper.rb
+++ rails/app/helpers/hub_helper.rb
@@ -7,9 +7,10 @@ module HubHelper
     hub_link_to(bi_icon(:tag) + tag_name, hub_tag_url(tag_name))
   end
 
-  def views_and_updated_text(hub_site)
+  def views_and_updated_text(hub_site, show_clone_count: false)
     [
-      "#{nice_view_count(hub_site.view_count)} views",
+      ("#{nice_view_count(hub_site.view_count)} views" unless show_clone_count),
+      ("#{nice_view_count(hub_site.clone_count)} clones" if show_clone_count),
       (brief_time_ago_in_words(hub_site.blob_created_at) if hub_site.blob_created_at && hub_site.save_count > 0)
     ].compact.join(', ')
   end
