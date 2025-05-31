033817b319e58c0c67645fe2639cdf73a0e3d1db
Simon Baird <simon.baird@gmail.com>
2025-05-31 17:22:50 -0400

    More syncing of rails defaults with app:update
    Update some rails boot and environment setup scripts to be closer to
    the rails defaults.
    
    Not confident I know all the details about the changes, but I'm
    asking a popular LLM for explanations, and so far it seems like it
    should be fine.
    
    There are still more changes to come, but I'm once again trying to
    cut this into smaller chunks and merge the low risk parts first.
    
    A bunch of this of this is just comment changes which of coures have
    no significant impact. Also some development and test environment
    changes should be low risk for breaking prod.

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -1,26 +1,26 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'active_storage/engine'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require 'action_view/railtie'
+require "action_view/railtie"
 # require "action_cable/engine"
-require 'sprockets/railtie'
-require 'rails/test_unit/railtie'
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Our custom settings handler. We have to load it early so the
 # settings can be used while we're still starting up rails.
-require_relative '../lib/settings'
+require_relative "../lib/settings"
 
 # Our custom logger
-require_relative '../app/middleware/logger_with_domain'
+require_relative "../app/middleware/logger_with_domain"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -29,11 +29,17 @@ Bundler.require(*Rails.groups)
 module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # Todo: Should this be updated to 7.1?
+    # Todo: Should be updated soon
     config.load_defaults 6.1
 
     # https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#new-activesupport-cache-serialization-format
+    # Todo: Should be removed carefully
     config.active_support.cache_format_version = 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
--- rails/config/environments/test.rb
+++ rails/config/environments/test.rb
@@ -1,4 +1,4 @@
-require 'active_support/core_ext/integer/time'
+require "active_support/core_ext/integer/time"
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
@@ -8,27 +8,25 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = false
-  config.action_view.cache_template_loading = true
+  # While tests run files are not watched, reloading is not necessary.
+  config.enable_reloading = false
 
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  # Eager loading loads your entire application. When running a single test locally,
+  # this is usually not necessary, and can slow down your test suite. However, it's
+  # recommended that you enable it in continuous integration systems to ensure eager
+  # loading is working properly before deploying your code.
+  config.eager_load = ENV["CI"].present?
 
   # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
-  }
+  config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{1.hour.to_i}" }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = :none
+  # Render exception templates for rescuable exceptions and raise for other exceptions.
+  config.action_dispatch.show_exceptions = :rescuable
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
@@ -36,6 +34,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
+  # Disable caching for Action Mailer templates even if Action Controller
+  # caching is enabled.
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
@@ -60,4 +60,7 @@ Rails.application.configure do
 
   # Added when updating to Rails 7.2
   config.active_job.queue_adapter = :test
+
+  # Raise error when a before_action's only/except options reference missing actions.
+  config.action_controller.raise_on_missing_callback_actions = true
 end
