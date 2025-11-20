c1ac63770c37800ae4290a2ed06fa7ad7ed36c24
Simon Baird <simon.baird@gmail.com>
2025-11-20 11:18:05 -0500

    Update pay and stripe gems to 8.x and 13.x
    Now we have some tests this should be less scary...
    
    Required a migration done like this:
    
        bin/rails pay:install:migrations
        bin/rails db:migrate

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_27_164510) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_20_162020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_27_164510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stripe_account"
+    t.string "type"
     t.index ["customer_id", "processor_id"], name: "index_pay_charges_on_customer_id_and_processor_id", unique: true
     t.index ["subscription_id"], name: "index_pay_charges_on_subscription_id"
   end
@@ -102,6 +103,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_27_164510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stripe_account"
+    t.string "type"
     t.index ["owner_type", "owner_id", "deleted_at", "default"], name: "pay_customer_owner_index"
     t.index ["processor", "processor_id"], name: "index_pay_customers_on_processor_and_processor_id", unique: true
   end
@@ -115,6 +117,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_27_164510) do
     t.jsonb "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
     t.index ["owner_type", "owner_id", "processor"], name: "index_pay_merchants_on_owner_type_and_owner_id_and_processor"
   end
 
@@ -122,11 +125,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_27_164510) do
     t.bigint "customer_id", null: false
     t.string "processor_id", null: false
     t.boolean "default"
-    t.string "type"
+    t.string "payment_method_type"
     t.jsonb "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stripe_account"
+    t.string "type"
     t.index ["customer_id", "processor_id"], name: "index_pay_payment_methods_on_customer_id_and_processor_id", unique: true
   end
 
@@ -152,6 +156,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_27_164510) do
     t.datetime "pause_resumes_at"
     t.string "payment_method_id"
     t.string "stripe_account"
+    t.string "type"
     t.index ["customer_id", "processor_id"], name: "index_pay_subscriptions_on_customer_id_and_processor_id", unique: true
     t.index ["metered"], name: "index_pay_subscriptions_on_metered"
     t.index ["pause_starts_at"], name: "index_pay_subscriptions_on_pause_starts_at"
