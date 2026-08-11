5bb9f3d62aa48f29fcb5c8b16ff1657e737b3cf5
Simon Baird <simon.baird@gmail.com>
2026-08-11 16:17:28 -0400

    Skip CSRF when signing out
    The motivation is for the experimental TiddlyWiki "Tiddlyhost Sign
    Out" button and http request I'm tinkering with.

--- rails/app/controllers/sessions_controller.rb
+++ rails/app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < Devise::SessionsController
+  skip_before_action :verify_authenticity_token, only: [:destroy]
   before_action :configure_sign_in_params, only: [:create]
 
   def destroy
